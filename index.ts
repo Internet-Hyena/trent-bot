de0cddb69338af923fa3a14c0e4f6984b73cf85d
Internet Hyena <mozzarella.she@pm.me>
2026-04-16 14:38:28 -0400

    Fork: Trent Reznor bot

--- index.ts
+++ index.ts
@@ -4,34 +4,37 @@ import { CronJob } from 'cron';
 import * as process from 'process';
 import * as fs from 'node:fs';
 import * as path from 'node:path';
+import { unescape } from 'node:querystring';
 
 dotenv.config();
 
 // Create a Bluesky Agent 
 const agent = new BskyAgent({
     service: 'https://bsky.social',
-  })
+})
 
 
 async function main() {
     // get a random file from the list
-    let randomFilePath: string;
-    let imageBlob : Blob;
+    let basename: string;
+    let imageBlob: Blob;
     do {
         const fileUrls = fs
-        .readFileSync(process.env.IMAGE_LIST_NAME!)
+            .readFileSync(process.env.IMAGE_LIST_NAME!)
             .toString('utf8')
             .split('\n');
-        randomFilePath = fileUrls[fileUrls.length * Math.random() | 0];
-        const fileUrl = process.env.IMAGE_URL_BASE! + randomFilePath;
+        const fileUrl = fileUrls[fileUrls.length * Math.random() | 0];
+        basename = fileUrl.slice(fileUrl.lastIndexOf('/') + 1);
+        basename = unescape(basename);
+
         console.log(`Fetching file... ${fileUrl}`);
         imageBlob = await (await fetch(fileUrl)).blob();
-        console.log(`Got ${randomFilePath} (size=${imageBlob.size},type=${imageBlob.type})`);
+        console.log(`Got ${basename} (size=${imageBlob.size},type=${imageBlob.type})`);
     } while (!imageBlob.type.startsWith("image/") || imageBlob.size > 976000)
-    
+
     console.log(`Logging in as ${process.env.BLUESKY_USERNAME!}...`);
-    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD!})
-    
+    await agent.login({ identifier: process.env.BLUESKY_USERNAME!, password: process.env.BLUESKY_PASSWORD! })
+
     console.log(`Uploading image as blob...`);
     const uploadBlobRespose = await agent.uploadBlob(await imageBlob.bytes(), {
         "encoding": "",
@@ -41,18 +44,20 @@ async function main() {
     });
     const blobRef = uploadBlobRespose.data.blob;
     console.log(`Successfully uploaded blob (ref=${blobRef.toJSON()})`);
-    
+
+    const postText = "File:" + basename.replaceAll("_", " ");
+
     const postResponse = await agent.post({
-        text: randomFilePath.toUpperCase(),
+        text: postText,
         embed: {
             $type: "app.bsky.embed.images",
             images: [{
-                alt: path.basename(randomFilePath),
+                alt: path.basename(basename),
                 image: blobRef
             }]
         }
     });
-    
+
     console.log(`Just posted! ${postResponse.cid}`);
 }
 
